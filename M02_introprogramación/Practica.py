7712ebe17107dcc82df635243c24eb198d0829f2
Humberto Sosa <138712846+hsm1969@users.noreply.github.com>
2023-08-02 17:45:29 -0500

    Add files via upload

--- M02_introprogramación/Practica.py
+++ M02_introprogramación/Practica.py
@@ -1,8 +1,3 @@
-
-# Este es un archivo de prueba, sin actividades =) .
-
-# A partir del próximo módulo: M03_variablesydatos vas a tener actividades para realizar.
-
 print('Hola Mundo!')
 a = 20
 while a > 0:
